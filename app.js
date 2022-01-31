a837c214ee438ef499e192f0e132e12211b4f893
Enes Demirtas <enesdemirtas255@gmail.com>
2022-01-31 21:15:22 +0300

    Template Engine is added...

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express")
-
+const ejs = require("ejs")
 const app = express()
 
+
+// Template Engine
+app.set("view engine", "ejs")
+
+// Middleware
+app.use(express.static("public"))
+
+// Routes
 app.get("/", (req, res) => {
+    res.render("index")
+})
+app.get("/about", (req, res) => {
+    res.render("about")
+})
 
-    const blog = {
-        id: 1,
-        name: "Blog Title",
-        description: "Blog Description"
-    }
+app.get("/add_post", (req, res) => {
+    res.render("add_post")
+})
 
-    res.send(blog)
+app.get("/post", (req, res) => {
+    res.render("post")
 })
 
 const port = 3000
