5715356477936f838ba5fb262f61e98ef09c1b59
Enes Demirtas <enesdemirtas255@gmail.com>
2022-02-02 22:48:02 +0300

    DB Connection is provided

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const ejs = require("ejs")
+const Post = require("./models/Post")
+
 const app = express()
 
+// Create DB Connection
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 // Template Engine
 app.set("view engine", "ejs")
 
 // Middleware
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // Routes
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/", async (req, res) => {
+    const add_post = await Post.find({})
+    res.render("index", {
+        add_post
+    })
 })
+
+
 app.get("/about", (req, res) => {
     res.render("about")
 })
@@ -23,6 +38,11 @@ app.get("/add_post", (req, res) => {
 
 app.get("/post", (req, res) => {
     res.render("post")
+})
+
+app.post("/add_post", async (req, res) => {
+    await Post.create(req.body)
+    res.redirect("/")
 })
 
 const port = 3000
