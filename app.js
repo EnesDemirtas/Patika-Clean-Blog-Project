7d6442cb01bad782a778a67bf5ac1988abda0373
Enes Demirtas <enesdemirtas255@gmail.com>
2022-02-06 02:23:17 +0300

    Update&Delete buttons are added and MVC structure is applied.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const methodOverride = require("method-override")
 const ejs = require("ejs")
-const Post = require("./models/Post")
 const { restart } = require("nodemon")
+const postController = require("./controllers/postController")
+const pageController = require("./controllers/pageController")
 
 const app = express()
 
@@ -19,38 +21,22 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(
+    methodOverride("_method", {
+        methods: ["POST", "GET"],
+    })
+)
 
 // Routes
-app.get("/", async (req, res) => {
-    const posts = await Post.find({})
-    res.render("index", {
-        posts
-    })
-})
+app.get("/", postController.getAllPosts)
+app.get("/posts/:id", postController.getPost)
+app.post("/add_post", postController.createPost)
+app.put("/posts/:id", postController.updatePost)
+app.delete("/posts/:id", postController.deletePost)
 
-app.get("/posts/:id", async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render("post", {
-        post
-    })
-})
-
-app.get("/about", (req, res) => {
-    res.render("about")
-})
-
-app.get("/add_post", (req, res) => {
-    res.render("add_post")
-})
-
-app.get("/post", (req, res) => {
-    res.render("post")
-})
-
-app.post("/add_post", async (req, res) => {
-    await Post.create(req.body)
-    res.redirect("/")
-})
+app.get("/posts/edit/:id", pageController.getEditPage)
+app.get("/about", pageController.getAboutPage)
+app.get("/add_post", pageController.getAddPage)
 
 const port = 3000
 app.listen(port, () => {
